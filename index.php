c7e9a0fc451d305429eec89b602c1f5b7aa69f1e
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-04-12 14:22:08 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP badwords</title>
+</head>
+<body>
+    <form action="./form.php" method="get"></form>
+    
+</body>
+</html>
